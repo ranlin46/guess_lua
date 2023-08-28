803d73b3639d4c44325121b5c51163af6066aa27
ranlin <ranlin0406@foxmail.com>
2023-08-28 13:01:00 +0800

    这是面向对象的版本

--- main.py
+++ main.py
@@ -1,33 +1,35 @@
 import random
 
-# 创建角色和数字
-characters = ["温", "顺", "乱"]
-numbers = ["1", "2", "3"]
 
-# 随机搭配角色和数字
-random.shuffle(characters)
+class GuessingGame:
+    def __init__(self):
+        self.characters = ["温", "顺", "乱"]
+        self.numbers = ["1", "2", "3"]
+        random.shuffle(self.characters)
+        self.character_to_number = {num: char for num, char in zip(self.numbers, self.characters)}
+        self.answers = {}
 
-# 创建角色和数字的映射
-character_to_number = {num: char for num, char in zip(numbers, characters)}
+    def generate_answers(self):
+        for num in self.numbers:
+            other_numbers = [n for n in self.numbers if n != num]
+            answer = random.choice(other_numbers)
+            self.answers[num] = self.character_to_number[answer]
 
-# 创建每个角色的回答
-answers = {}
-for num in numbers:
-    other_numbers = [n for n in numbers if n != num]
-    answer = random.choice(other_numbers)
-    answers[num] = character_to_number[answer]
+    def play_game(self):
+        self.generate_answers()
 
-# 输出每个角色的回答
-for num, answer in answers.items():
-    print(f"{num}说：{answer}")
+        for num, answer in self.answers.items():
+            print(f"{num}说：{answer}")
 
-guess = int(input("请输入你猜测是乱的角色的序号： "))
-# 获取玩家猜测的角色对应的字符
-guessed_character = character_to_number.get(str(guess))
+        guess = int(input("请输入你猜测是乱的角色的序号： "))
+        guessed_character = self.character_to_number.get(str(guess))
 
-# 判断玩家猜测是否正确
-if guessed_character == "乱":
-    print("恭喜！你猜对了。")
-else:
-    print("很抱歉，你猜错了。")
+        if guessed_character == "乱":
+            print("恭喜！你猜对了。")
+        else:
+            print("很抱歉，你猜错了。")
 
+
+if __name__ == "__main__":
+    game = GuessingGame()
+    game.play_game()
